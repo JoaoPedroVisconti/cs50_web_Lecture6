5a4d27e1ced1f3275bc8516e2558f07623292f07
JoaoPedroVisconti <60099922+JoaoPedroVisconti@users.noreply.github.com>
2020-05-01 17:06:36 +0100

    until post2

---
+++ post0/static/index.js
@@ -0,0 +1,53 @@
+
+// Start with first post
+let counter = 1;
+
+// Load posts 20 at time
+const quantity = 20;
+
+// When DOM loads, render the first 20 posts
+document.addEventListener('DOMContentLoaded', load);
+
+// If scrolled to bottom, load the next 20 post
+window.onscroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        load()
+    }
+}
+
+function load() {
+
+    // Set start and end post numbers, and update counter
+    const start = counter;
+    const end = start + quantity - 1;
+    counter = end +1;
+
+    // Open new request to get new posts
+    const request = new XMLHttpRequest();
+    request.open('POST', '/posts');
+    request.onload = () =>  {
+        const data = JSON.parse(request.responseText);
+        data.forEach(add_post);     
+    };
+
+    // Add start end points to request data
+    const data = new FormData();
+    data.append('start', start);
+    data.append('end', end);
+
+    // Send request
+    request.send(data);
+
+};
+
+// Add a new post with given content to DOM
+function add_post(contents) {
+    
+    // Create new post
+    const post = document.createElement('div');
+    post.className = 'post';
+    post.innerHTML = contents;
+
+    // Add post to DOM
+    document.querySelector('#posts').append(post);
+};
